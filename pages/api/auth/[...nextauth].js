ac9ca9fb2357ede92520d6673e3774aa7c69da95
unknown <zshake11@gmail.com>
2022-04-03 22:56:20 +0530

    next auth changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,25 +7,11 @@ export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
         })
         // ...add more providers here
     ]
