61d653cc7a2f41236fd41402da8ad3e4b0d9f437
unknown <zshake11@gmail.com>
2022-11-10 11:02:10 +0530

    ccav wip

--- pages/api/ccavRequestHandler.js
+++ pages/api/ccavRequestHandler.js
@@ -1,9 +1,7 @@
-var http = require('http'),
-    fs = require('fs'),
-    ccav = require('./ccavutil.js'),
-    qs = require('querystring');
+const ccav = require('./ccavutil.js')
 
-export default function handler(req, res) {
+
+export default function (req, res) {
     var body = '',
         workingKey = '1C1A069B7FD2CEF791F42561377C9A4F',		//Put in the 32-Bit key shared by CCAvenues.
         accessCode = '	AVCT46JJ15BR82TCRB',		//Put in the access code shared by CCAvenues.
@@ -11,16 +9,14 @@ export default function handler(req, res) {
         formbody = '',
         merchant_id = '1529688'
 
-    body += req.data;
-    encRequest = ccav.encrypt(body, workingKey);
-    let POST = qs.parse(body);
+    body = new URLSearchParams(req.body);
+
+    encRequest = ccav.encrypt(body.toString(), workingKey);
+
+    // POST = qs.parse(body);
     formbody = '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=' + merchant_id + '&encRequest=' + encRequest + '&access_code=' + accessCode + '"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
 
-    // request.on('end', function () {
-    //     response.writeHeader(200, { "Content-Type": "text/html" });
-    //     response.write(formbody);
-    //     response.end();
-    // });
+
     res.status(200).send(formbody)
     return;
 };
--- pages/payment.js
+++ pages/payment.js
@@ -5,7 +5,32 @@ const payment = () => {
     const [iframe, setIframe] = useState()
 
     useEffect(() => {
-        axios.post('/api/ccavRequestHandler')
+        axios.post('/api/ccavRequestHandler', {
+            merchant_id: '1529688',
+            amount: "1.00",
+            order_id: '4334',
+            currency: "INR",
+            billing_name: "zed",
+            billing_address: "panvel",
+            billing_city: "mumbai",
+            billing_state: "MH",
+            billing_zip: '410206',
+            billing_country: 'India',
+            billing_tel: '8454015530',
+            billing_email: 'zshake11@gmail.com',
+            delivery_name: 'zed',
+            delivery_address: "panvel",
+            delivery_city: "mumbai",
+            delivery_state: "MH",
+            delivery_zip: '410206',
+            delivery_country: 'India',
+            delivery_tel: '8454015530',
+            integration_type: 'iframe_normal',
+            customer_identifier: '44870',
+            redirect_url: '/api/ccavResponseHandler',
+            cancel_url: '/api/ccavResponseHandler',
+            language: 'EN'
+        })
             .then(res => setIframe(res.data))
             .catch(err => console.log(err))
     }, [])
