15bdb0f3b471c0ecb0a0d8ff52a3b3f82d48b81b
unknown <zshake11@gmail.com>
2022-11-18 18:54:55 +0530

    ccav wip

--- pages/api/ccavResponseHandler.js
+++ pages/api/ccavResponseHandler.js
@@ -1,20 +1,18 @@
 const ccav = require('./ccavutil.js')
 
 export default function (req, res) {
-    console.log("req ========", req)
-    console.log("req.data=============", req.data)
-    console.log("req.body==========", req.body)
     var ccavEncResponse = '',
         ccavResponse = '',
         workingKey = '0E3C7C18D465713163159A1A5E8AE19A',	//Put in the 32-Bit key provided by CCAvenues.
         ccavPOST = '';
-    console.log("level 1 =====")
+
     ccavEncResponse += JSON.stringify(req.body);
     console.log("🚀 ~ file: ccavResponseHandler.js ~ line 13 ~ ccavEncResponse", ccavEncResponse)
-    ccavPOST = new URLSearchParams(ccavEncResponse);
-    console.log("level 2 =====")
-    console.log("🚀 ~ file: ccavResponseHandler.js ~ line 15 ~ ccavPOST", ccavPOST)
-    var encryption = ccavPOST.toString().encResp;
+    ccavPOST = new URLSearchParams(ccavEncResponse.encResp);
+
+    console.log("🚀 ~ file: ccavResponseHandler.js ~ line 15 ~ ccavPOST", ccavPOST.encResp)
+    console.log("----------------------------")
+    var encryption = ccavPOST.encResp;
     console.log("level 3 =====")
     ccavResponse = ccav.decrypt(encryption, workingKey);
     console.log("level 4 =====")
@@ -24,6 +22,6 @@ export default function (req, res) {
     pData = pData.replace(/&/gi, '</td></tr><tr><td>')
     pData = pData + '</td></tr></table>'
     htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' + pData + '</center><br></body></html>';
-
-    res.status(200).send(formbody)
+    console.log("level 5 =====")
+    res.status(200).send(htmlcode)
 };
