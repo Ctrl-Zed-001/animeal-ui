eb470fdd1a63c3a4f8fb532b4d979a7d9354a8f9
unknown <zshake11@gmail.com>
2023-04-08 11:16:23 +0530

    wip

--- pages/topcategories/index.js
+++ pages/topcategories/index.js
@@ -19,19 +19,19 @@ const TopCategories = () => {
             <div className="container mb-10">
                 <h1 className='text-xl font-medium mb-6'>Top categories</h1>
                 <div className="grid grid-cols-2 xl:grid-cols-4 gap-10">
-                    <CategoryBox placeholder='/img/icons/dog.webp' animal="dog" category={{ name: "Dog Food", slug: "food" }} />
-                    <CategoryBox placeholder='/img/icons/cat.webp' animal="cat" category={{ name: "Cat Food", slug: "food" }} />
-                    <CategoryBox placeholder='/img/icons/medicines.webp' animal="dog" category={{ name: "Dog Medicine", slug: "medicine" }} />
-                    <CategoryBox placeholder='/img/icons/treats.webp' animal="dog" category={{ name: "Dog Treats", slug: "treats" }} />
-                    <CategoryBox placeholder='/img/icons/toys.webp' animal="cat" category={{ name: "Cat Supplies", slug: "supplies" }} />
-                    <CategoryBox placeholder='/img/icons/supplements.webp' animal="dog" category={{ name: "Dog Supplements", slug: "supplements" }} />
-                    <CategoryBox placeholder='/img/icons/premium-food.webp' animal="cat" category={{ name: "Cat Treats", slug: "treats" }} />
-                    <CategoryBox placeholder='/img/icons/cat-wet-food.webp' animal="dog" category={{ name: "Dog Supplies", slug: "supplies" }} />
+                    <CategoryBox placeholder='/img/icons/dog.webp' animal="dog" category={{ attributes: { name: "Dog Food", slug: "food" } }} />
+                    <CategoryBox placeholder='/img/icons/cat.webp' animal="cat" category={{ attributes: { name: "Cat Food", slug: "food" } }} />
+                    <CategoryBox placeholder='/img/icons/medicines.webp' animal="dog" category={{ attributes: { name: "Dog Medicine", slug: "medicine" } }} />
+                    <CategoryBox placeholder='/img/icons/treats.webp' animal="dog" category={{ attributes: { name: "Dog Treats", slug: "treats" } }} />
+                    <CategoryBox placeholder='/img/icons/toys.webp' animal="cat" category={{ attributes: { name: "Cat Supplies", slug: "supplies" } }} />
+                    <CategoryBox placeholder='/img/icons/supplements.webp' animal="dog" category={{ attributes: { name: "Dog Supplements", slug: "supplements" } }} />
+                    <CategoryBox placeholder='/img/icons/premium-food.webp' animal="cat" category={{ attributes: { name: "Cat Treats", slug: "treats" } }} />
+                    <CategoryBox placeholder='/img/icons/cat-wet-food.webp' animal="dog" category={{ attributes: { name: "Dog Supplies", slug: "supplies" } }} />
                 </div>
 
             </div>
             {
-                brands ?
+                brands && brands.length > 0 ?
                     <Brands brands={brands} title={"Popular Brands"} /> :
                     <></>
             }
